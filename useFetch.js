a41dd9e0451f30dfe0d341de05c0e060ec45b4ab
Aarón RJ (Developer) <aresinodeveloper@gmail.com>
2020-07-07 07:54:21 +0200

    First commit

---
+++ useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({ loading: false, error: null, data });
+        }
+      })
+      .catch(() =>
+        setState({
+          data: null,
+          loading: true,
+          error: "No se pudo cargar la info",
+        })
+      );
+  }, [url]);
+
+  return state;
+};
